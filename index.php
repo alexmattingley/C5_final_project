336d8a3636aea8aec5e456c58d8126900510f7aa
Alex Mattingley <alexmattingley@gmail.com>
2015-09-07 13:00:50 -0700

    working navigation

--- index.php
+++ index.php
@@ -2,13 +2,38 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>C5 final project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <!-- jQuery library -->
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <script src="main.js"></script>
 </head>
 <body>
+    <div class="hero_banner">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="glyphicon glyphicon-menu-hamburger"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse left-side-nav navbar-left" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="index.php#work">Work</a></li>
+                        <li><a href="index.php#contact">Contact</a></li>
+                </div>
+                <div class="right-side-nav pull-right">
+                    <button class="btn btn-default">Login</button>
+                </div>
+            </div>
+        </nav>
 
+    </div>
 </body>
 </html>
