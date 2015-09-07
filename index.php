55391688f39e9b016f6adc23a5e0dc97ee5ab379
Alex Mattingley <alexmattingley@gmail.com>
2015-09-07 15:22:44 -0700

    moved the script tags

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-    <script src="main.js"></script>
 </head>
 <body>
     <div class="hero_banner">
@@ -77,5 +73,8 @@
         </div>
         <div class="clearfix"></div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="main.js"></script>
 </body>
 </html>
