18c6ce15b4a510e43cd4349fa95db7ec6971840c
Alex Mattingley <alexmattingley@gmail.com>
2015-09-14 14:33:09 -0700

    closes #20. starts #21

--- data_handlers/wunder_curl_request.php
+++ data_handlers/wunder_curl_request.php
@@ -24,9 +24,12 @@
 //$json_decode = json_decode($curl_response);
 //print_r($json_decode);
 
-
+/********************************************
+ * pulling Goleta wind/temp readings.
+ */
 $json_string = file_get_contents("http://api.wunderground.com/api/b249567299fad989/geolookup/conditions/q/CA/goleta.json");
 $parsed_json = json_decode($json_string);
+$goleta_location = $parsed_json->{'location'}->{'city'};
 $goleta_temp_f = $parsed_json->current_observation->temp_f;
 $goleta_wind_dir = $parsed_json->current_observation->wind_dir;
 $golelta_wind_mph= $parsed_json->current_observation->wind_mph;
@@ -35,6 +38,7 @@ $goleta_weather = $parsed_json->current_observation->weather;
 $goleta_last_observed = $parsed_json->current_observation->observation_time;
 
 $line_break = '<br>';
+print("Located in: $goleta_location $line_break");
 print("Air Temp: $goleta_temp_f $line_break");
 print("Wind direction: $goleta_wind_dir $line_break");
 print("Wind Speed(mph): $golelta_wind_mph $line_break");
@@ -44,6 +48,8 @@ print("Last Reading: $goleta_last_observed $line_break");
 
 print_r($parsed_json->current_observation);
 
-
+/************************
+ * Sending Goleta data to database.
+ */
 
 ?>
