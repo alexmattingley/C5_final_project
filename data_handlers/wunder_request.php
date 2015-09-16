aa89916d8c4c28a290b30765e33c0631061a4933
Alex Mattingley <alexmattingley@gmail.com>
2015-09-16 12:28:44 -0700

    query information changed to identify column to be changed by station_id rather than location, making the proccess better

--- data_handlers/wunder_request.php
+++ data_handlers/wunder_request.php
@@ -39,7 +39,7 @@ $i = 0;
 if(mysqli_num_rows($results) > 0){
     while($result = mysqli_fetch_assoc($results)){
        $api_url = $result['api_url'];
-       print($api_url);
+       //print($api_url);
     }
 }
 
@@ -52,6 +52,8 @@ if(mysqli_num_rows($results) > 0){
 $json_string = file_get_contents("$api_url");
 $parsed_json = json_decode($json_string);
 $location = $parsed_json->{'location'}->{'city'};
+$observation_location = $parsed_json->current_observation->observation_location->city;
+$station_id = $parsed_json->current_observation->station_id;
 $temp_f = $parsed_json->current_observation->temp_f;
 $wind_dir = $parsed_json->current_observation->wind_dir;
 $wind_mph= $parsed_json->current_observation->wind_mph;
@@ -62,6 +64,8 @@ $last_observed = $parsed_json->current_observation->observation_time;
 $line_break = '<br>';
 
 //the print statements below are simply to make sure the request is still working
+print("observation location: $observation_location $line_break");
+print("station id: $station_id $line_break");
 print("Located in: $location $line_break");
 print("Air Temp: $temp_f $line_break");
 print("Wind direction: $wind_dir $line_break");
@@ -76,12 +80,11 @@ print("Last Reading: $last_observed $line_break");
  */
 
 require('/Applications/MAMP/htdocs/Sandbox/C5_final_project/mysql_connect.php');
-$query = "UPDATE `wind_data` SET `last_observed`='$last_observed',`temp_f`='$temp_f',`wind_dir`='$wind_dir',`wind_mph`='$wind_mph',`wind_gust_mph`='$wind_gust',`weather`='$weather',`location`='$location',`last_updated`= UNIX_TIMESTAMP(now()) WHERE `location`='$location'";
+$query = "UPDATE `wind_data` SET `last_observed`='$last_observed',`temp_f`='$temp_f',`wind_dir`='$wind_dir',`wind_mph`='$wind_mph',`wind_gust_mph`='$wind_gust',`weather`='$weather',`last_updated`= UNIX_TIMESTAMP(now()) WHERE `station_id`='$station_id'";
 $results = mysqli_query($conn, $query);
 if (mysqli_affected_rows($conn) > 0) {
     print_r($results);
 }else {
-    print($location);
     print('something isnt working');
 }
 
