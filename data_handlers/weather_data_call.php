1fb6c4db4167d81d6c9c0fa4e259363cd5e00b57
Alex Mattingley <alexmattingley@gmail.com>
2015-09-16 12:54:00 -0700

    closes #27. creating array of objects returned from the wind_data table

--- data_handlers/weather_data_call.php
+++ data_handlers/weather_data_call.php
@@ -3,11 +3,12 @@ require('../mysql_connect.php');
 $query = "SELECT * FROM `wind_data`";
 $results = mysqli_query($conn, $query);
 $i = 0;
+$weather_array = array();
 if(mysqli_num_rows($results) > 0){
     while($result = mysqli_fetch_assoc($results)){
-       $json_string = json_encode($result);
+        array_push($weather_array, $result);
     }
 }
-
+$json_string = json_encode($weather_array);
 print($json_string);
 ?>
