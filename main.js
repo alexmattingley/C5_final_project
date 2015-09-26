126d492098d6043db405d2e40a700083341cf704
Alex Mattingley <alexmattingley@gmail.com>
2015-09-26 13:27:21 -0700

    building chart with dummy data, gonna work on getting the data from noaa to poulate into the chart

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ $(document).ready(function(){
     cdip_get_data();
     wunderground_data_call();
 
-   $(".location-sub-menu li a").click(function(){ //this needs to be more scalable
+   $(".location-sub-menu li a").click(function(){
        var loc_id = $(this).attr('loc_id');
        pull_relevant_buoy_by_location(loc_id);
+
    });
 
    get_tide_data();
@@ -292,9 +293,9 @@ function pull_relevant_buoy_by_location(location_id){
             location_index: location_id
         },
         success: function(response){
-            console.log(response);
             remove_content();
             $content_container.html(response);
+            build_buoy_chart();
         }
 
     });
@@ -323,84 +324,86 @@ function remove_content() {
  */
 
 
-// Get context with jQuery - using jQuery's .get() method.
-var my_chart_node = $("#myChart").get(0);
-var ctx = my_chart_node.getContext("2d"); //$("#myChart").get(0) is undefined, meaning it doesnt exist yet.
-var myLineChart = new Chart(ctx).Line(data, options);
+function build_buoy_chart(){
+    var my_chart_node = $("#myChart").get(0);
+    var ctx = my_chart_node.getContext("2d");
+    console.log("ctx: ",ctx);
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
 
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
+    var options = {
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
 
-var options = {
-    ///Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : true,
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
 
-    //String - Colour of the grid lines
-    scaleGridLineColor : "rgba(0,0,0,.05)",
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
 
-    //Number - Width of the grid lines
-    scaleGridLineWidth : 1,
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
 
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
 
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
+        //Boolean - Whether the line is curved between points
+        bezierCurve : true,
 
-    //Boolean - Whether the line is curved between points
-    bezierCurve : true,
+        //Number - Tension of the bezier curve between points
+        bezierCurveTension : 0.4,
 
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension : 0.4,
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
 
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 4,
 
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 4,
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 1,
 
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
+        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius : 20,
 
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
 
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 2,
 
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : true,
 
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
-    //String - A legend template
-    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+    };
+    var myLineChart = new Chart(ctx).Line(data, options);
+}
 
-};
 
 
 /**************
