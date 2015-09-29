b006608eebf245dcc4c7887f831760cec85417a5
Alex Mattingley <alexmattingley@gmail.com>
2015-09-29 14:33:17 -0700

    minor styling stuff done. closes #53, #54, and #55

--- data_handlers/default_location_page.php
+++ data_handlers/default_location_page.php
@@ -146,6 +146,7 @@ function create_indiv_wind(){
     </div>
     <div class="tidal-predictions">
         <div class="container-fluid col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+            <h3>Tides for today and tomorrow</h3>
             <canvas id="myChart"></canvas>
         </div>
         <div class="clearfix"></div>
