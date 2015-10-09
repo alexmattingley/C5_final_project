a2c7965bfd457d15d334e8448c7602689c130d32
Alex Mattingley <alexmattingley@gmail.com>
2015-10-08 17:09:45 -0700

    little stuff

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
         </div>
         <div class="clearfix"></div>
         <div class="hero_banner">
-            <div class="hero-text text-center col-xs-10 col-xs-offset-1">
-                <img class = "mobile-logo col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3" src="images/logo.png" alt="">
-                <h3 class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">A no frills tool to help you get barreled.</h3>
+            <div class="hero-text text-center col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+                <img class = "mobile-logo " src="images/logo.png" alt="">
+                <h3 class="">A no frills tool to help you get barreled.</h3>
             </div>
         </div>
         <div class="location-tabs">
