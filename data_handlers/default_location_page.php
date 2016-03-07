958a5dee510160e7c711967c3a9105c6e6d630c5
Alex Mattingley <alexmattingely@gmail.com>
2016-03-07 11:26:04 -0800

    created a custom border for a 3 column layout

--- data_handlers/default_location_page.php
+++ data_handlers/default_location_page.php
@@ -49,7 +49,7 @@ function create_indiv_wind(){
 
         <?php } else {; ?>
 
-        <div class="indiv-wind col-xs-10 col-xs-offset-1 col-sm-3">
+        <div class="indiv-wind custom-space col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">
 
         <?php }; ?>
 
@@ -89,13 +89,15 @@ function create_indiv_wind(){
         <div class="clearfix"></div>
     </div>
     <div class="wind-data-block">
-        <div class="container-fluid col-lg-8 col-lg-offset-2">
-            <h3 class="col-xs-10 col-xs-offset-1 col-lg-12 col-sm-offset-0">Wind</h3>
-            <?php create_indiv_wind(); ?>
-            <p class="credit col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">Weather Data provided by <a href="http://www.wunderground.com/">Wunderground</a></p>
+        <div class="container">
+            <div class="col-lg-10 col-lg-offset-1">
+                <h3>Wind</h3>
+                <?php create_indiv_wind(); ?>
+                <p class="credit col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-0">Weather Data provided by <a href="http://www.wunderground.com/">Wunderground</a></p>
+                <div class="clearfix"></div>
+            </div>
             <div class="clearfix"></div>
         </div>
-        <div class="clearfix"></div>
     </div>
     <div class="buoy-predictions">
         <div class="text-container col-xs-10 col-xs-offset-1 col-lg-6 col-lg-offset-3">
